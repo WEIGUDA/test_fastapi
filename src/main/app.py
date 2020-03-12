1ebcf60d76f9e41efdf5eceaebb6f0bc92753362
Desmond Chen <desmondddd@gmail.com>
2020-03-12 23:33:18 +0800

    updated app.py

--- src/main/app.py
+++ src/main/app.py
@@ -16,6 +16,16 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/health")
+def health():
+    return "UP"
+
+
+@app.get("/live")
+def live():
+    return "UP"
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
